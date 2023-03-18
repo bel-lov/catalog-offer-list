797a2c2e3c4e58ac3e341461e028de90c0e2c39d
bel-lov <belou.lyuba@yandex.ru>
2023-03-18 17:53:14 +0300

    first commit

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default function ListItem(props) {
+    const { listing_id, url, MainImage, title, currency_code, price, quantity } = props;
+
+    let name = title.length > 50 ? `${title.slice(0, 50)}...` : title;
+    let code = currency_code;
+    let levelClass;
+    let priceCurrency;
+
+    if (currency_code === 'USD') {
+        priceCurrency = `$${price}`;
+    } else if (currency_code === 'EUR') {
+        priceCurrency = `€${price}`;
+    } else {
+        priceCurrency = `${price} ${currency_code}`;
+    }
+
+    if (quantity <= 10) {
+        levelClass = 'level-low';
+    } else if (quantity <= 20) {
+        levelClass = 'level-medium';
+    } else {
+        levelClass = 'level-high';
+    }
+
+    return (
+        <>
+            <div className="item-list">
+                <div className="item">
+                    <div className="item-image">
+                        <a href={url}>
+                            <img src={MainImage.url_570xN} />
+                        </a>
+                    </div>
+                    <div className="item-details">
+                        <p className="item-title">{name}</p>
+                        <p className="item-price">{price}</p>
+                        <p className={levelClass}>{quantity}</p>
+                    </div>
+                </div>
+            </div >
+        </>
+    );
+}
---
+++ src/components/Listing.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ListItem from "./ListItem";
+
+
+export default function Listing(props) {
+    const { items } = props;
+    return (
+        <div className="item-list">
+            {items.map((item) => (
+                <ListItem key={item.listing_id} item={item} />
+            ))}
+        </div>
+    )
+}
+
+Listing.defaultProps = {
+    items: [],
+};
